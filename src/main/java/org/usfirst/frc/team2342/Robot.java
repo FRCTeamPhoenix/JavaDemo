40789aa8d499b9b112f2bdd3e7be982eb115ead4
IanCoolidge0 <ian.coolidge@asdnh.org>
2017-09-16 15:15:20 -0400

    Initial code commit

---
+++ src/main/java/org/usfirst/frc/team2342/Robot.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team2342;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+/**
+ * Created by cooli on 5/6/2017.
+ */
+public class Robot extends SampleRobot {
+
+    
+
+    public Robot()
+    {
+
+    }
+
+    @Override
+    public void robotInit() {
+        super.robotInit();
+    }
+
+    @Override
+    public void autonomous() {
+        super.autonomous();
+    }
+
+    @Override
+    public void operatorControl() {
+        super.operatorControl();
+    }
+
+    @Override
+    public void test() {
+        super.test();
+    }
+}
